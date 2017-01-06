ccea22b31c48dc54afa72141cc88336a0b78923f
spencerhallam <spencerhallam@gmail.com>
2017-01-05 22:56:02 -0600

    Update MainController.js

--- MainController.js
+++ MainController.js
@@ -4,8 +4,8 @@ myApp.controller('MainController', ['$scope', function($scope) {
      income2: 350, 
      rent: 645, 
      utilities: 35, 
-     cargas: "The Empty Hearse", 
-     carinsurance: "NOTES HERE", 
+     cargas: 50, 
+     carinsurance: 103.50, 
      datetime: new Date(2014, 11, 31, 21, 00, 00, 00)
   };
 }]);
