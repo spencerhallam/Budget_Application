ec5b14fd9392ffa22aad04dffe094672301264ff
spencerhallam <spencerhallam@gmail.com>
2017-01-05 23:34:23 -0600

    Update MainController.js

--- MainController.js
+++ MainController.js
@@ -9,8 +9,8 @@ myApp.controller('MainController', ['$scope', function($scope) {
      carinsurance: 103.50,
      cargas: 50,  
      datetime: new Date(2014, 11, 31, 21, 00, 00, 00),
-     totalmonthlyincome: function(){return 2 + 2;}
   };
+  $scope.totalmonthlyincome = function(){return 2 + 2;}
 }]);
 
 //app.controller('MainController', ['$scope', function($scope) {
