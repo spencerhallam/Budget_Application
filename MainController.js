bef204e729d970d5a06020385ce1e153686f80ab
spencerhallam <spencerhallam@gmail.com>
2017-01-05 22:39:45 -0600

    Update MainController.js

--- MainController.js
+++ MainController.js
@@ -1,13 +1,23 @@
 app.controller('MainController', ['$scope', function($scope) {
   $scope.monthly = { 
-     income_1: 1500, 
-     income_2: 350, 
+     income1: 1500, 
+     income2: 350, 
      rent: 645, 
      utilities: 35, 
      cargas: "The Empty Hearse", 
      carinsurance: "NOTES HERE", 
      datetime: new Date(2014, 11, 31, 21, 00, 00, 00)
   };
-  
-  
 }]);
+
+//app.controller('MainController', ['$scope', function($scope) {
+//  $scope.program = { series: "Sherlock", 
+//                    series_img: "img/sherlock.jpg", 
+//                    genre: "Crime drama", 
+//                    season: 3, 
+//                    episode: "The Empty Hearse", 
+//                    description: "ts and  Night.", 
+//                    datetime: new Date(2014, 11, 31, 21, 00, 00, 00) 
+//  };
+//  
+// }]);
