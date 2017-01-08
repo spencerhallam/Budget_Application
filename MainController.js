91c1a33641e1b7a10e1c4f6391340d44867cad5a
spencerhallam <spencerhallam@gmail.com>
2017-01-07 22:32:18 -0600

    Update MainController.js

--- MainController.js
+++ MainController.js
@@ -1,7 +1,7 @@
 myApp.controller('MainController', ['$scope', function($scope) { 
-     $scope.income1 = [350, 500, 1200];
-     $scope.income2 = [350, 500, 1200];
-     $scope.income3 = [350, 500, 1200];
+     $scope.income1 = 1200;//[350, 500, 1200];
+     $scope.income2 = 350;//[350, 500, 1200];
+     $scope.income3 = 500;//[350, 500, 1200];
      $scope.food = [250, 350, 450];
      $scope.rent = 645;
      $scope.utilities = 35;
