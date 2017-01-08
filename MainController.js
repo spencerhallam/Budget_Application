da68150f87aadbf9d5e33dc2f09f72c2389c284e
spencerhallam <spencerhallam@gmail.com>
2017-01-08 11:45:20 -0600

    Update MainController.js

--- MainController.js
+++ MainController.js
@@ -2,10 +2,10 @@ myApp.controller('MainController', ['$scope', function($scope) {
      $scope.income1_options = [100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000, 1050, 1100, 1150, 1200, 1250, 1300, 1350, 1400];
      $scope.income2_options = [100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000, 1050, 1100, 1150, 1200, 1250, 1300, 1350, 1400];
      $scope.income3_options = [100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000, 1050, 1100, 1150, 1200, 1250, 1300, 1350, 1400];
-     $scope.income1 = 1200; //[350, 500, 1200];
-     $scope.income2 = 350; //[350, 500, 1200];
-     $scope.income3 = 500; //[350, 500, 1200];
-     $scope.food = 450; //[250, 350, 450];
+     $scope.income1 = 1200;
+     $scope.income2 = 350; 
+     $scope.income3 = 500; 
+     $scope.food = 300; 
      $scope.rent = 645;
      $scope.utilities = 35;
      $scope.carinsurance = 103.50;
@@ -19,17 +19,4 @@ myApp.controller('MainController', ['$scope', function($scope) {
         $scope.descincome = $scope.mymonthlyincome - $scope.monthlyexpenses;  
      };
      $scope.calculations();
-
 }]);
-
-//app.controller('MainController', ['$scope', function($scope) {
-//  $scope.program = { series: "Sherlock", 
-//                    series_img: "img/sherlock.jpg", 
-//                    genre: "Crime drama", 
-//                    season: 3, 
-//                    episode: "The Empty Hearse", 
-//                    description: "ts and  Night.", 
-//                    datetime: new Date(2014, 11, 31, 21, 00, 00, 00) 
-//  };
-//  
-// }]);
