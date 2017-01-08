1edab0ec21e6cf0cec379afbe8410aa165140c2b
spencerhallam <spencerhallam@gmail.com>
2017-01-07 22:25:11 -0600

    Update MainController.js

--- MainController.js
+++ MainController.js
@@ -1,12 +1,12 @@
 myApp.controller('MainController', ['$scope', function($scope) { 
-     $scope.income1: [350, 500, 1200];
-     $scope.income2: [350, 500, 1200];
-     $scope.income3: [350, 500, 1200];
-     $scope.food: [250, 350, 450];
-     $scope.rent: 645;
-     $scope.utilities: 35;
-     $scope.carinsurance: 103.50;
-     $scope.cargas: 50;
+     $scope.income1 = [350, 500, 1200];
+     $scope.income2 = [350, 500, 1200];
+     $scope.income3 = [350, 500, 1200];
+     $scope.food = [250, 350, 450];
+     $scope.rent = 645;
+     $scope.utilities = 35;
+     $scope.carinsurance = 103.50;
+     $scope.cargas = 50;
      $scope.calculations = function(){
          
         $scope.mybiweeklyincome = $scope.income1 + $scope.income2 + $scope.income3;
