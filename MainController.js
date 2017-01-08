f354d51241a424d1ffacf7170b1be883db555df3
spencerhallam <spencerhallam@gmail.com>
2017-01-07 21:19:19 -0600

    Update MainController.js

--- MainController.js
+++ MainController.js
@@ -1,15 +1,17 @@
-myApp.controller('MainController', ['$scope', function($scope) {
-  $scope.monthly = { 
-     income1: 1500, 
-     income2: 350,
-     income3: 350,
-     food: 400,
-     rent: 645, 
-     utilities: 35, 
-     carinsurance: 103.50,
-     cargas: 50,  
-     datetime: new Date(2014, 11, 31, 21, 00, 00, 00),
-  };
+myApp.controller('MainController', ['$scope', function($scope) { 
+     $scope.income1: 1500, 
+     $scope.income2: 350,
+     $scope.income3: 350,
+     $scope.food: 400,
+     $scope.rent: 645, 
+     $scope.utilities: 35, 
+     $scope.carinsurance: 103.50,
+     $scope.cargas: 50,  
+     $scope.calculations = function(){
+     
+    
+     };
+
 }]);
 
 //app.controller('MainController', ['$scope', function($scope) {
