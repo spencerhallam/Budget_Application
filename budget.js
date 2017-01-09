66f1a188d5b0a984addef55a5dd1e27b9f5bf0ef
spencerhallam <spencerhallam@gmail.com>
2017-01-09 16:29:03 -0600

    Update budget.js

--- budget.js
+++ budget.js
@@ -2,33 +2,33 @@ $(document).ready(function(){
    $('.show-01, .show-02, .show-03, .show-04, .show-05, .show-06, .show-07, .show-08, .show-09, .show-10').hide();
    alert("working!");
    $('.on-01').on("click", function(){
-        $('.show-01').toggle();
+        $('.show-01').show();
    });
    $('.on-02').on("click", function(){
-        $('.show-02').toggle();
+        $('.show-02').show();
    });
    $('.on-03').on("click", function(){
-        $('.show-03').toggle();
+        $('.show-03').show();
    });
    $('.on-04').on("click", function(){
-        $('.show-04').toggle();
+        $('.show-04').show();
    });
    $('.on-05').on("click", function(){
-        $('.show-05').toggle();
+        $('.show-05').show();
    });
    $('.on-06').on("click", function(){
-        $('.show-06').toggle();
+        $('.show-06').show();
    });
    $('.on-07').on("click", function(){
-        $('.show-07').toggle();
+        $('.show-07').show();
    });
    $('.on-08').on("click", function(){
-        $('.show-08').toggle();
+        $('.show-08').show();
    });
    $('.on-09').on("click", function(){
-        $('.show-09').toggle();
+        $('.show-09').show();
    });
    $('.on-10').on("click", function(){
-        $('.show-10').toggle();
+        $('.show-10').show();
    });
 });
