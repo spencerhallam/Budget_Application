29ca85685f795cd5927810a9d6d8a4eb33978f94
spencerhallam <spencerhallam@gmail.com>
2017-01-09 16:15:24 -0600

    Update budget.js

--- budget.js
+++ budget.js
@@ -4,7 +4,34 @@ $(document).ready(function(){
   $(".income").on("mouseover", function(){
         $('body').addClass("gored");
    });
-   $('li').on("mouseover", function(){
-        $('.show-select').toggle();
+   $('.on-01').on("click", function(){
+        $('.show-01').toggle();
+   });
+   $('.on-02').on("click", function(){
+        $('.show-02').toggle();
+   });
+   $('.on-03').on("click", function(){
+        $('.show-03').toggle();
+   });
+   $('.on-04').on("click", function(){
+        $('.show-04').toggle();
+   });
+   $('.on-05').on("click", function(){
+        $('.show-05').toggle();
+   });
+   $('.on-06').on("click", function(){
+        $('.show-06').toggle();
+   });
+   $('.on-07').on("click", function(){
+        $('.show-07').toggle();
+   });
+   $('.on-08').on("click", function(){
+        $('.show-08').toggle();
+   });
+   $('.on-09').on("click", function(){
+        $('.show-09').toggle();
+   });
+   $('.on-10').on("click", function(){
+        $('.show-10').toggle();
    });
 });
