f79d7e28b5379404ae54c16983f94aca2fc7d36d
spencerhallam <spencerhallam@gmail.com>
2017-01-09 15:54:40 -0600

    Update budget.js

--- budget.js
+++ budget.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
+   $('.show-select').hide();
    alert("working!");
   $(".income").on("mouseover", function(){
         $('body').addClass("gored");
    });
    $('.calc-form li').on("mouseover", function(){
-        $('.show-select').show();
+        $('.show-select').toggle();
    });
 });
