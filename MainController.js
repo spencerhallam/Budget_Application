c4875f2dbfc605d40ce989c4b2444167c9bd2175
spencerhallam <spencerhallam@gmail.com>
2017-01-08 19:16:40 -0600

    Update MainController.js

--- MainController.js
+++ MainController.js
@@ -1,10 +1,21 @@
 myApp.controller('MainController', ['$scope', function($scope) { 
-     $scope.income1_options = [100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000, 1050, 1100, 1150, 1200, 1250, 1300, 1350, 1400];
-     $scope.income2_options = [100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000, 1050, 1100, 1150, 1200, 1250, 1300, 1350, 1400];
-     $scope.income3_options = [100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000, 1050, 1100, 1150, 1200, 1250, 1300, 1350, 1400];
-     $scope.income1 = 1200;
-     $scope.income2 = 350; 
-     $scope.income3 = 500; 
+     $scope.income1_options = [0, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000, 1050, 1100, 1150, 1200, 1250, 1300, 1350, 1400];
+     $scope.income2_options = [0, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000, 1050, 1100, 1150, 1200, 1250, 1300, 1350, 1400];
+     $scope.income3_options = [0, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000, 1050, 1100, 1150, 1200, 1250, 1300, 1350, 1400];
+     $scope.food_options = 300; 
+     $scope.rent_options = 645;
+     $scope.utilities_options = 35;
+     $scope.carinsurance_options = 103.50;
+     $scope.cargas_options = 50;
+     $scope.studentloanpmt_options = 250;
+     $scope.phone_options = 110;
+     $scope.internet_options = 35;
+     $scope.creditcard1_options = 0;
+     $scope.creditcard2_options = 0;
+     
+     $scope.income1 = 600;
+     $scope.income2 = 0; 
+     $scope.income3 = 0;
      $scope.food = 300; 
      $scope.rent = 645;
      $scope.utilities = 35;
@@ -13,6 +24,8 @@ myApp.controller('MainController', ['$scope', function($scope) {
      $scope.studentloanpmt = 250;
      $scope.phone = 110;
      $scope.internet = 35;
+     $scope.creditcard1 = 0;
+     $scope.creditcard2 = 0;
      $scope.calculations = function(){
          
         $scope.mybiweeklyincome = $scope.income1 + $scope.income2 + $scope.income3;
