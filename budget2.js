851a3367df2322a7cee2f8824d8145b967b38619
spencerhallam <spencerhallam@gmail.com>
2017-01-10 09:06:34 -0600

    Update budget2.js

--- budget2.js
+++ budget2.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
    $('li select').hide();
    alert("working!");
-   $('form.ul li')each().on("click", function(){
+   $('form.ul li').each().on("click", function(){
         $('li select').show();
    });
 
